cd65e9ec4907a96ebf3290b2c48c42a58eb1aa91
Aidosgd <aidosgd@gmail.com>
2017-09-11 08:38:28 +0600

    fix

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -41,7 +41,7 @@
                         ];
                         $price_d = $price[$category->id] / $currencies;
                     ?>
-                    <div class="product__price">{{ number_format($price[$category->id], 0, ' ', ' ') }} тг</div>
+                    <div class="product-page__price">{{ number_format($price[$category->id], 0, ' ', ' ') }} тг</div>
                     <div class="product-page__price-dollar">~ {{ number_format($price_d, 0, ',', ' ') }} $</div>
                     <button data-toggle="modal" data-target="#orders" class="btn btn-default">Заказать</button>
                 </div>
@@ -53,33 +53,33 @@
             </div>
 
             <div class="row product-descriptions">
-                <div class="col-md-6">
+                <div class="col-md-6 border-right">
                     <ul>
-                        <li><span>Бренд:</span> <span class="right">{{ $fields->options['options']['ru'][$manufacturer[$product->category->id]] }}</span></li>
-                        <li><span>Коллекция:</span> <span class="right">{{ $product->node->title }}</span></li>
-                        @if($category->id == 1)<li><span>Кому:</span> <span class="right">{{ $fields3->options['options']['ru'][$product->node->fields->product_sex] }}</span></li>@endif
+                        <li><span>Бренд:</span> <span class="right">{{ $fields->options['options']['ru'][$manufacturer[$product->category->id]] }}</span><div class="clearfix"></div></li>
+                        <li><span>Коллекция:</span> <span class="right">{{ $product->node->title }}</span><div class="clearfix"></div></li>
+                        @if($category->id == 1)<li><span>Кому:</span> <span class="right">{{ $fields3->options['options']['ru'][$product->node->fields->product_sex] }}</span><div class="clearfix"></div></li>@endif
                         <?php
                             $type = [
                                     1 => isset($product->node->fields->product_type) ? $product->node->fields->product_type : '',
                                     2 => isset($product->node->fields->product_type_2) ? $product->node->fields->product_type_2 : '',
                             ];
                         ?>
-                        @if($category->id != 3)<li><span>Тип:</span> <span class="right">{{ $fields4->options['options']['ru'][$type[$product->category->id]] }}</span></li>@endif
-                        @if($category->id == 1)<li><span>Материал корпуса:</span> <span class="right">{{ $fields2->options['options']['ru'][$product->node->fields->product_material_case] }}</span></li>@endif
-                        @if($category->id == 1)<li><span>Водопроницаемость:</span> <span class="right">{{ $product->node->fields->watertightness }}</span></li>@endif
+                        @if($category->id != 3)<li><span>Тип:</span> <span class="right">{{ $fields4->options['options']['ru'][$type[$product->category->id]] }}</span><div class="clearfix"></div></li>@endif
+                        @if($category->id == 1)<li><span>Материал корпуса:</span> <span class="right">{{ $fields2->options['options']['ru'][$product->node->fields->product_material_case] }}</span><div class="clearfix"></div></li>@endif
+                        @if($category->id == 1)<li><span>Водопроницаемость:</span> <span class="right">{{ $product->node->fields->watertightness }}</span><div class="clearfix"></div></li>@endif
                     </ul>
                 </div>
                 <div class="col-md-6">
                     <ul>
-                        @if($category->id == 1)<li><span>Диаметр корпуса:</span> <span class="right">{{ $product->node->fields->case_diameter }}</span></li>
-                        <li><span>Комплектация:</span> <span class="right">{{ $product->node->fields->equipment }}</span></li>
-                        <li><span>Цвет циферблата:</span> <span class="right">{{ $product->node->fields->dial_color }}</span></li>
-{{--                        <li><span>Тип механизма:</span> <span class="right">{{ $product->node->fields->product_condition }}</span></li>--}}
-{{--                        <li><span>Запас хода:</span> <span class="right">{{ $product->node->fields->product_condition }}</span></li>--}}
-{{--                        <li><span>Калибр:</span> <span class="right">{{ $product->node->fields->product_condition }}</span></li>--}}
-                        <li><span>Состояние:</span> <span class="right">{{ $fields5->options['options']['ru'][$product->node->fields->product_condition] }}</span></li>
-                        <li><span>Материал ремешка:</span> <span class="right">{{ $fields6->options['options']['ru'][$product->node->fields->strap_material] }}</span></li>
-                        <li><span>Функции:</span> <span class="right">{{ $product->node->fields->function }}</span></li>@endif
+                        @if($category->id == 1)<li><span>Диаметр корпуса:</span> <span class="right">{{ $product->node->fields->case_diameter }}</span><div class="clearfix"></div></li>
+                        <li><span>Комплектация:</span> <span class="right">{{ $product->node->fields->equipment }}</span><div class="clearfix"></div></li>
+                        <li><span>Цвет циферблата:</span> <span class="right">{{ $product->node->fields->dial_color }}</span><div class="clearfix"></div></li>
+{{--                        <li><span>Тип механизма:</span> <span class="right">{{ $product->node->fields->product_condition }}</span>div.clearfix</li>--}}
+{{--                        <li><span>Запас хода:</span> <span class="right">{{ $product->node->fields->product_condition }}</span>div.clearfix</li>--}}
+{{--                        <li><span>Калибр:</span> <span class="right">{{ $product->node->fields->product_condition }}</span>div.clearfix</li>--}}
+                        <li><span>Состояние:</span> <span class="right">{{ $fields5->options['options']['ru'][$product->node->fields->product_condition] }}</span><div class="clearfix"></div></li>
+                        <li><span>Материал ремешка:</span> <span class="right">{{ $fields6->options['options']['ru'][$product->node->fields->strap_material] }}</span><div class="clearfix"></div></li>
+                        <li><span>Функции:</span> <span class="right">{{ $product->node->fields->function }}</span><div class="clearfix"></div></li>@endif
                     </ul>
                 </div>
             </div>
